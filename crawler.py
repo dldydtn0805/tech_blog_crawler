1064ca51c57cce8476cf72c3a447ec1e1962465e
dldydtn0805 <ssafysoo95@gmail.com>
2023-12-13 23:04:29 +0900

    1

--- crawler.py
+++ crawler.py
@@ -1,4 +1,3 @@
-import requests
 # selenium의 webdriver를 사용하기 위한 import
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,15 +11,24 @@ import time
 # 크롬드라이버 실행
 driver = webdriver.Chrome() 
 
-url = 'https://tech.kakao.com/blog/'
-#크롬 드라이버에 url 주소 넣고 실행
-driver.get(url)
+# 카카오 블로그 크롤링, 제목, 링크, 날짜 출력
+def kakao_crawling():
+    url = 'https://tech.kakao.com/blog/'
+    #크롬 드라이버에 url 주소 넣고 실행
+    driver.get(url)
+    # 페이지가 완전히 로딩되도록 3초동안 기다림
+    time.sleep(3)
+    i = 1
+    news = []
+    while True:
+        try:
+            new = driver.find_element(By.XPATH, f"/html/body/div[2]/div/section[3]/div/div/div[1]/div/div/div[3]/div/div[1]/article[{i}]/div/h3/a")
+            date = driver.find_element(By.XPATH, f"/html/body/div[2]/div/section[3]/div/div/div[1]/div/div/div[3]/div/div[1]/article[{i}]/div/div[1]/span[2]")    
+            news.append([new.text, new.get_attribute('href'), date.text])
+            i += 1
+        except:
+            break
+    return news
 
-# 페이지가 완전히 로딩되도록 3초동안 기다림
-time.sleep(3)
-
-news_titles = driver.find_elements(By.CLASS_NAME, "elementor-post__title")
-for news_title in news_titles:
-    print(news_title.text)
-    
-
+kakao_news = kakao_crawling()
+print(kakao_news)
